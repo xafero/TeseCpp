10a623f5027cdd586a87280f7196f6aeb302e2c5
xafero <georg.berendt@uni-oldenburg.de>
2016-10-29 02:59:07 +0200

    Added core test

--- TeseCpp.Tests/TeseTest.h
+++ TeseCpp.Tests/TeseTest.h
@@ -2,11 +2,22 @@
 #pragma once
 
 using namespace System;
+using namespace NUnit::Framework;
 
 namespace TeseCppTests {
 
+	[TestFixture]
 	public ref class TeseTest 
 	{
+	public:
+		[SetUp]
+		void TestSetup();
+		[TearDown]
+		void TestTeardown();
+		[Test]
+		void TestDeserialize();
+		[Test]
+		void TestSerialize();
 	};
 
 }
