a791fad107af2bb671494d9748daa634a982354e
xafero <georg.berendt@uni-oldenburg.de>
2016-10-29 05:34:04 +0200

    Implemented address properties

--- TeseCpp.Tests/Customer.cpp
+++ TeseCpp.Tests/Customer.cpp
@@ -26,3 +26,45 @@ TeseCppTests::Customer::Customer(long id, String^ firstName, String^ lastName,
 	this->birth = birth;
 	this->home = home;
 }
+
+long TeseCppTests::Customer::Id::get() { return this->id; }
+Void TeseCppTests::Customer::Id::set(long value) { this->id = value; }
+
+String^ TeseCppTests::Customer::FirstName::get() { return this->firstName; }
+Void TeseCppTests::Customer::FirstName::set(String^ value) { this->firstName = value; }
+
+String^ TeseCppTests::Customer::LastName::get() { return this->lastName; }
+Void TeseCppTests::Customer::LastName::set(String^ value) { this->lastName = value; }
+
+double TeseCppTests::Customer::Money::get() { return this->money; }
+Void TeseCppTests::Customer::Money::set(double value) { this->money = value; }
+
+bool TeseCppTests::Customer::Male::get() { return this->male; }
+Void TeseCppTests::Customer::Male::set(bool value) { this->male = value; }
+
+char TeseCppTests::Customer::Sex::get() { return this->sex; }
+Void TeseCppTests::Customer::Sex::set(char value) { this->sex = value; }
+
+int TeseCppTests::Customer::Houses::get() { return this->houses; }
+Void TeseCppTests::Customer::Houses::set(int value) { this->houses = value; }
+
+short TeseCppTests::Customer::Pets::get() { return this->pets; }
+Void TeseCppTests::Customer::Pets::set(short value) { this->pets = value; }
+
+float TeseCppTests::Customer::Crazyness::get() { return this->crazyness; }
+Void TeseCppTests::Customer::Crazyness::set(float value) { this->crazyness = value; }
+
+Byte TeseCppTests::Customer::Bits::get() { return this->bits; }
+Void TeseCppTests::Customer::Bits::set(Byte value) { this->bits = value; }
+
+BigInteger TeseCppTests::Customer::Sleep::get() { return this->sleep; }
+Void TeseCppTests::Customer::Sleep::set(BigInteger value) { this->sleep = value; }
+
+Decimal TeseCppTests::Customer::Awake::get() { return this->awake; }
+Void TeseCppTests::Customer::Awake::set(Decimal value) { this->awake = value; }
+
+DateTime^ TeseCppTests::Customer::Birth::get() { return this->birth; }
+Void TeseCppTests::Customer::Birth::set(DateTime^ value) { this->birth = value; }
+
+TeseCppTests::Address^ TeseCppTests::Customer::Home::get() { return this->home; }
+Void TeseCppTests::Customer::Home::set(Address^ value) { this->home = value; }
--- TeseCpp.Tests/Customer.h
+++ TeseCpp.Tests/Customer.h
@@ -30,6 +30,20 @@ namespace TeseCppTests {
 		Customer(long id, String^ firstName, String^ lastName, double money, bool male, 
 			char sex, int houses, short pets, float crazyness, Byte bits,
 			BigInteger sleep, Decimal awake, DateTime^ birth, Address^ home);
+		property long Id { long get(); void set(long value); }
+		property String^ FirstName { String^ get(); void set(String^ value); }
+		property String^ LastName { String^ get(); void set(String^ value); }
+		property double Money { double get(); void set(double value); }
+		property bool Male { bool get(); void set(bool value); }
+		property char Sex { char get(); void set(char value); }
+		property int Houses { int get(); void set(int value); }
+		property short Pets { short get(); void set(short value); }
+		property float Crazyness { float get(); void set(float value); }
+		property Byte Bits { Byte get(); void set(Byte value); }
+		property BigInteger Sleep { BigInteger get(); void set(BigInteger value); }
+		property Decimal Awake { Decimal get(); void set(Decimal value); }
+		property DateTime^ Birth { DateTime^ get(); void set(DateTime^ value); }
+		property Address^ Home { Address^ get(); void set(Address^ value); }
 	};
 
 }
