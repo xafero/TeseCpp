c5deb4d9990683ef63ddc6e85312a22d01227467
xafero <georg.berendt@uni-oldenburg.de>
2016-10-29 06:19:04 +0200

    Fixed tese builder

--- TeseCpp/TeseBuilder.cpp
+++ TeseCpp/TeseBuilder.cpp
@@ -4,9 +4,14 @@
 #include "TeseBuilder.h"
 
 TeseCpp::TeseBuilder^ TeseCpp::TeseBuilder::SkipNull(bool skipNull) {
+	this->skipNull = skipNull;
 	return this;
 }
 
 TeseCpp::Tese^ TeseCpp::TeseBuilder::Create() {
 	return gcnew TeseCpp::Tese();
+}
+
+bool TeseCpp::TeseBuilder::IsSkipNull::get() {
+	return this->skipNull;
 }
--- TeseCpp/TeseBuilder.h
+++ TeseCpp/TeseBuilder.h
@@ -9,9 +9,12 @@ namespace TeseCpp {
 
 	public ref class TeseBuilder 
 	{
+	private:
+		bool skipNull;
 	public:
 		TeseBuilder^ SkipNull(bool skipNull);
 		Tese^ Create();
+		property bool IsSkipNull { bool get(); } 
 	};
 
 }
